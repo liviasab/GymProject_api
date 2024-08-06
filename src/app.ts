881c4dff2a7a3f8efe47edb5fe76ad5b3f78e052
liviasab <liviasabino92@gmail.com>
2024-08-06 20:30:03 -0300

    refactor: create file for controls and routes

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,7 @@
 import fastify from "fastify"
-import { z } from 'zod'
-import { prisma } from "./lib/prisma"
-
-// import { stat } from "fs";
-
-import bcrypt from 'bcrypt'
+import { appRoutes } from "./http/routes"
 
 export const app = fastify()
 
 
-app.post('/users', async (request, reply) => {
-    const registerSchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(8),
-    });
-    const { name, email, password } = registerSchema.parse(request.body)
-
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds)
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: hashedPassword,
-        }
-    });
-    return reply.status(201).send();
-
-});
+app.register(appRoutes)
---
+++ src/http/controllers/register.ts
@@ -0,0 +1,26 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+import { bcrypt } from 'bcrypt';
+
+export async function register (request: FastifyRequest, reply: FastifyReply) {
+  const registerSchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+      password: z.string().min(8),
+  });
+  const { name, email, password } = registerSchema.parse(request.body)
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+  await prisma.user.create({
+      data: {
+          name,
+          email,
+          password_hash: hashedPassword,
+      }
+  });
+  return reply.status(201).send();
+
+};
