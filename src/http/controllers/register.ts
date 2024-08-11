6cbf6cef4fa0ac400e7391674019ac6f2b577abb
liviasab <liviasabino92@gmail.com>
2024-08-11 14:33:28 -0300

    refactor: reversing prisma dependencies

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
-import { registerUserCase } from '@/use-cases/register';
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { RegisterUseCase } from '@/use-cases/register';
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from "zod";
 
@@ -12,7 +13,10 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerSchema.parse(request.body)
 
   try {
-    await registerUserCase ({
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+    
+    await registerUseCase.execute ({
         name,
         email,
         password,
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,6 @@
 import bcrypt, { hash }  from "bcrypt";
-import { prisma } from "@/lib/prisma";
-import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { UsersRepository } from "@/repositories/users-repository";
+
 
 
 interface RegisterUserCaseRequest {
@@ -8,30 +8,33 @@ interface RegisterUserCaseRequest {
   email: string
   password: string
 }
-export async function registerUserCase({
-  name,
-  email,
-  password,
-}: RegisterUserCaseRequest) {
-  const password_hash = await hash(password, 6)
-  
-  
 
-const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-        email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
+export class RegisterUseCase{
+  constructor( private usersRepository: UsersRepository) {}
+  
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUserCaseRequest) {
+    const password_hash = await hash(password, 6)
+    
+    
+  
+  const userWithSameEmail = await this. usersRepository.findByEmail(email)
+  
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+   
+   await this.usersRepository.create({
+    name,
+    email,
+    password_hash,
+   })
+    
   }
- const prismaUsersRepository = new PrismaUsersRepository()
-
- await prismaUsersRepository.create({
-  name,
-  email,
-  password_hash,
- })
   
 }
+
+
