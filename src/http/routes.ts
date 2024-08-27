87a287baa588f65e394acc1e5d29018a82da86d5
enrell <enrellsa10@proton.me>
2024-08-27 20:06:33 -0300

    📝 (routes.ts): add new route '/process-qr' to handle QR code processing, enhancing the application functionality for users who prefer using their mobile devices as a form of authentication or data input method. This change is made in response to user feedback requesting more convenient ways to interact with our service beyond traditional web forms and login methods.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
+import { processQRCode } from "./controllers/process-qr-code";
+
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.post('/process-qr', processQRCode)
 }
