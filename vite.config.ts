0fd87a7f7c2dd3ab91d16574c662e0585d4ccdee
enrell <enrellsa10@proton.me>
2024-09-10 16:08:34 -0300

    Refactor Vite configuration file

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig(async () => {
-  const tsconfigPaths = (await import('vite-tsconfig-paths')).default;
-
-  return {
-    plugins: [tsconfigPaths()],
-  };
-});
