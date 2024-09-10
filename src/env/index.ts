af2b805c93c77d4f9321624582c4e8cceae99b61
enrell <enrellsa10@proton.me>
2024-09-10 16:09:12 -0300

    Refactor env/index.ts to handle invalid environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,19 @@
 import 'dotenv/config'
 import { z } from 'zod'
 
-
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
+  // ... outras variáveis de ambiente ...
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success == false){
-  console.error('Invalid environment variables', _env.error.format())
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
 
-  throw new Error ('Invalid enviroment variable.')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
