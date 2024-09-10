c40223f36d4c4221dbcf732a5c9ea8a1ea938f46
enrell <enrellsa10@proton.me>
2024-09-10 16:09:19 -0300

    Refactor routes.ts to add gym-related routes and authentication middleware

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,38 @@ import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register";
 import { authenticate } from "./controllers/authenticate";
 import { processQRCode } from "./controllers/process-qr-code";
+import { create, read, update, remove, fetchUserGyms } from "./controllers/gyms";
+import { verifyJWT } from "./middlewares/verify-jwt";
+import { verifyUserRole } from "./middlewares/verify-user-role";
+import { addTurnstile, getTurnstileQRCode, getTurnstileCheckIns, listGymTurnstiles } from "./controllers/turnstiles";
+import { registerToGym } from "./controllers/gym-members";
+import { deleteTurnstile } from "./controllers/turnstiles";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', register)
-  app.post('/sessions', authenticate)
-  app.post('/process-qr', processQRCode)
+  app.post('/signup', register);
+  app.post('/login', authenticate);
+  
+  // Authenticated routes
+  app.post('/process-qr', { onRequest: [verifyJWT] }, processQRCode);
+  
+  // Gym CRUD routes
+  app.post('/gyms', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, create);
+  app.get('/gyms/:id', { onRequest: [verifyJWT] }, read);
+  app.put('/gyms/:id', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, update);
+  app.delete('/gyms/:id', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, remove);
+  app.get('/user/gyms', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, fetchUserGyms);
+
+  // New routes for gym owners
+  app.post('/gyms/:gymId/turnstiles', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, addTurnstile);
+  app.get('/turnstiles/:id/qrcode', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, getTurnstileQRCode);
+  app.get('/turnstiles/:id/check-ins', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, getTurnstileCheckIns);
+
+  // New route for gym members
+  app.post('/gyms/:id/register', { onRequest: [verifyJWT] }, registerToGym);
+
+  // New route to list gym turnstiles
+  app.get('/gyms/:gymId/turnstiles', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, listGymTurnstiles);
+
+  // Nova rota para deletar uma catraca
+  app.delete('/turnstiles/:id', { onRequest: [verifyJWT, verifyUserRole('GYM_OWNER')] }, deleteTurnstile);
 }
