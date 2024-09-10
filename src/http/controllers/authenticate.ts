4611af4442e15a8a31e873e4341c14f8a0bd530e
enrell <enrellsa10@proton.me>
2024-09-10 16:09:24 -0300

    Refactor authenticate controller to include JWT token generation

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,25 +1,37 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
-import { AuthenticateUseCase } from '@/use-cases/authenticate';
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error';
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case';
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from "zod";
 
-
-export async function authenticate (request: FastifyRequest, reply: FastifyReply) {
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
   const authenticateBodySchema = z.object({
-      email: z.string().email(),
-      password: z.string().min(6),
+    email: z.string().email(),
+    password: z.string().min(6),
   });
   const { email, password } = authenticateBodySchema.parse(request.body)
 
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
     
-    await authenticateUseCase.execute ({
-        email,
-        password,
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
     })
+
+    const token = await reply.jwtSign(
+      {
+        role: user.role
+      },
+      {
+        sign: {
+          sub: user.id
+        }
+      }
+    )
+
+    return reply.status(200).send({
+      token
+    });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({message: err.message})
@@ -27,7 +39,4 @@ export async function authenticate (request: FastifyRequest, reply: FastifyReply
 
     throw err
   }
-  
-  return reply.status(200).send();
-
 }
