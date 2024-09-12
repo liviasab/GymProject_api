aff35f459249094f2cd509a99613dc5abcd9a5ad
enrell <enrellsa10@proton.me>
2024-09-12 19:48:24 -0300

    Refactor delete-gym.ts to delete associated turnstiles before deleting the gym

--- src/use-cases/delete-gym.ts
+++ src/use-cases/delete-gym.ts
@@ -1,4 +1,5 @@
 import { GymsRepository } from "@/repositories/gyms-repository";
+import { TurnstilesRepository } from "@/repositories/turnstiles-repository";
 
 interface DeleteGymUseCaseRequest {
   id: string;
@@ -6,7 +7,10 @@ interface DeleteGymUseCaseRequest {
 }
 
 export class DeleteGymUseCase {
-  constructor(private gymsRepository: GymsRepository) {}
+  constructor(
+    private gymsRepository: GymsRepository,
+    private turnstilesRepository: TurnstilesRepository
+  ) {}
 
   async execute({ id, userId }: DeleteGymUseCaseRequest): Promise<void> {
     const isOwner = await this.gymsRepository.checkOwnership(id, userId);
@@ -15,6 +19,13 @@ export class DeleteGymUseCase {
       throw new Error('User is not the owner of this gym');
     }
 
+    // Primeiro, delete todas as catracas associadas
+    const turnstiles = await this.turnstilesRepository.findManyByGymId(id);
+    for (const turnstile of turnstiles) {
+      await this.turnstilesRepository.delete(turnstile.id);
+    }
+
+    // Depois, delete a academia
     await this.gymsRepository.delete(id);
   }
 }
